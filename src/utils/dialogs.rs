b3b329a1a113ee7b55a19b7573c5b22055cf0043
dennssen <dennssen@gmail.com>
2026-02-25 10:44:27 +0100

    Improved dialog helpers
    - added "buttons" parameter for message_dialog
    
    - fixed message_dialog not creating a message dialog if it had no parent window
    
    - message_dialog now returns MessageDialogResult
    
    - added option_dialog, a message dialog with yes or no buttons

--- src/utils/dialogs.rs
+++ src/utils/dialogs.rs
@@ -1,7 +1,7 @@
 use std::fmt::Display;
 use std::io::Error;
 use std::sync::{Arc, Mutex};
-use rfd::{MessageButtons, MessageDialog, MessageLevel};
+use rfd::{MessageButtons, MessageDialog, MessageDialogResult, MessageLevel};
 use imgui_winit_support::winit::window::Window;
 
 pub static WINDOW: Mutex<Option<Arc<Window>>> = Mutex::new(None);
@@ -10,36 +10,45 @@ pub fn error_dialog<S>(title: S, description: S, e: &Error)
 where
     S: Into<String> + Display
 {
-    message_dialog(title, format!("{}\n{}", description, e), MessageLevel::Error);
+    message_dialog(title, format!("{}\n{}", description, e), MessageLevel::Error, MessageButtons::Ok);
 }
 
 pub fn warn_dialog<S>(title: S, description: S)
 where
     S: Into<String> + Display
 {
-    message_dialog(title, description, MessageLevel::Warning);
+    message_dialog(title, description, MessageLevel::Warning, MessageButtons::Ok);
 }
 
 pub fn info_dialog<S>(title: S, description: S)
 where
     S: Into<String> + Display
 {
-    message_dialog(title, description, MessageLevel::Info);
+    message_dialog(title, description, MessageLevel::Info, MessageButtons::Ok);
 }
 
-fn message_dialog<S, D>(title: S, description: D, level: MessageLevel)
+pub fn option_dialog<S>(title: S, description: S) -> MessageDialogResult
+where
+    S: Into<String> + Display
+{
+    message_dialog(title, description, MessageLevel::Info, MessageButtons::YesNo)
+}
+
+fn message_dialog<S, D>(title: S, description: D, level: MessageLevel, buttons: MessageButtons) -> MessageDialogResult
 where
     S: Into<String>,
     D: Into<String>
 {
+    let mut message_dialog = MessageDialog::new()
+        .set_title(title)
+        .set_description(description)
+        .set_level(level)
+        .set_buttons(buttons);
+
     let window_lock = WINDOW.lock().unwrap();
     if let Some(window) = window_lock.as_ref() {
-        MessageDialog::new()
-            .set_level(level)
-            .set_title(title)
-            .set_description(description)
-            .set_buttons(MessageButtons::Ok)
-            .set_parent(window.as_ref())
-            .show();
+        message_dialog = message_dialog.set_parent(window.as_ref());
     }
+
+    message_dialog.show()
 }
