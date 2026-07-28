43fef2b72f1d46d5d16e7f548311f41a89c9cb8e
dennssen <dennssen@gmail.com>
2026-07-28 15:20:02 +0200

    Changed directory name to match script name
    Instead of matching the project name which was mostly meant for the git repo, i've changed it to match the script name. This makes more sense for the current workflow.

--- src/gui/screens/project_info.rs
+++ src/gui/screens/project_info.rs
@@ -85,7 +85,7 @@ pub fn project_info_screen(
                             drift_project.package_info.script_name = suggested_script_name;
                         }
 
-                        drift_project.directory_name = drift_project.package_info.project_name.replace(" ", "-");
+                        drift_project.directory_name = drift_project.package_info.script_name.replace(" ", "-");
 
                         let result = get_custom_templates();
 
